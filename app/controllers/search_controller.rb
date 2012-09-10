a9b9883d4c71902111f72ba5dcb88449baa8edc1
Jordan Leigh <scizorbot3@dhcp-251-200-215.nat.wireless.uic.edu>
2012-09-09 23:56:06 -0400

    Now opens in new window

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,24 +8,22 @@ require 'mechanize'
     
     agent = Mechanize.new
     
-     ticker_symbol = params[:ticker_symbol].upcase
-     page = agent.get("http://sec.gov/cgi-bin/browse-edgar?company=&match=&CIK=#{ticker_symbol}&filenum=&State=&Country=&SIC=&owner=exclude&Find=Find+Companies&action=getcompany")
+    ticker_symbol = params[:ticker_symbol].upcase
+    page = agent.get("http://sec.gov/cgi-bin/browse-edgar?company=&match=&CIK=#{ticker_symbol}&filenum=&State=&Country=&SIC=&owner=exclude&Find=Find+Companies&action=getcompany")
     
-     target_href = ""
+    target_href = ""
     
-     page.parser.xpath("//tr").each_with_index do |tr, index| 
-        if tr.children.first.children.first.text == "10-K"
-          target_href = "http://www.sec.gov/#{tr.children[2].children.first.attributes["href"].value}"
-        end
-     end
+    page.parser.xpath("//tr").each_with_index do |tr, index| 
+      if tr.children.first.children.first.text == "10-K"
+        target_href = "http://www.sec.gov/#{tr.children[2].children.first.attributes["href"].value}"
+      end
+    end
     
-     page = agent.get(target_href)
-     page = page.links[9].click
-     url = page.uri.to_s
-          
-     redirect_to url
-     
-     #render 'index'   
+    page = agent.get(target_href)
+    page = page.links[9].click
+    @url = page.uri.to_s
+    
+    redirect_to @url
   end
   
 end
