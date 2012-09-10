6812d60425addb3a25df065572554feacfbabb8f
Jordan Leigh <scizorbot3@dhcp-251-200-215.nat.wireless.uic.edu>
2012-09-10 00:47:07 -0400

    Got it working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,21 +9,27 @@ require 'mechanize'
     agent = Mechanize.new
     
     ticker_symbol = params[:ticker_symbol].upcase
-    page = agent.get("http://sec.gov/cgi-bin/browse-edgar?company=&match=&CIK=#{ticker_symbol}&filenum=&State=&Country=&SIC=&owner=exclude&Find=Find+Companies&action=getcompany")
+    page = agent.get("http://sec.gov/cgi-bin/browse-edgar?company=&match=&CIK=#{ticker_symbol}&filenum=&State=&Country=&SIC=&owner=exclude&Find=Find+Companies&action=getcompany&count=100")
     
     target_href = ""
-    
-    page.parser.xpath("//tr").each_with_index do |tr, index| 
-      if tr.children.first.children.first.text == "10-K"
-        target_href = "http://www.sec.gov/#{tr.children[2].children.first.attributes["href"].value}"
+      
+      page.parser.xpath("//tr").each_with_index do |tr, index| 
+        if tr.children.first.children.first.text == "10-K"
+          target_href = "http://www.sec.gov/#{tr.children[2].children.first.attributes["href"].value}"
+          break
+        end
       end
-    end
     
-    page = agent.get(target_href)
-    page = page.links[9].click
-    @url = page.uri.to_s
+      unless target_href == ""
+      page = agent.get(target_href)
+      page = page.links[9].click
+      @url = page.uri.to_s
+      redirect_to @url
+      return
+      end
+      
+    render :text => "Sorry, I couldn't find a 10-K from that ticker name. Double check to make sure it was entered correctly"
     
-    redirect_to @url
   end
   
 end
